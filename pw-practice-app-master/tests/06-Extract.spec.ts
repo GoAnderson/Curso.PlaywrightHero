81e5ac21384b085ea15a667e4487251b712d5702
GoAnderson <goandersonguima@gmail.com>
2024-09-11 09:01:26 -0300

    Extract values

---
+++ pw-practice-app-master/tests/06-Extract.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test('Extract', async ({ page }) => {
+
+    //Single test value
+    const basicForm = page.locator('nb-card').filter({ hasText: "Basic form" })
+    const buttonTest = await basicForm.locator('button').textContent()
+    expect(buttonTest).toEqual('Submit')
+
+    //All text values
+    const allRadioButtonsLabels = await page.locator('nb-radio').allTextContents()
+    expect(allRadioButtonsLabels).toContain("Option 1")
+
+    //Input value
+    const emailField = basicForm.getByRole('textbox', { name: "Email" })
+    await emailField.fill('test@test.com')
+    const emailValue = await emailField.inputValue()
+    expect(emailValue).toEqual('test@test.com')
+
+    const placeholderValue = await emailField.getAttribute('placeholder')
+    expect(placeholderValue).toEqual('Email')
+
+})
